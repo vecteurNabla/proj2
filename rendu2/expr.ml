cd18f7fc233b57c0637d7b604a861181cb20dccf
RJ <juder.orbion@gmail.com>
2021-02-13 19:32:18 +0100

    pas d'expressions bool a part + let _ (rq pas fait de tests :-<)

--- rendu2/expr.ml
+++ rendu2/expr.ml
@@ -17,18 +17,16 @@ type expr =
   | FunRec of string*var*expr     (* rec fun x -> e *)
   | App of expr*expr              (* e1 e2 *)
 
-  | If of ebool*expr*expr         (* if b then e1 else e2 *)
-
   | Unit                          (* () *)
   | Seq of expr * expr            (* e1; e2 *)
   | Aff of expr * expr            (* e1 := e2 *)
   | Der of expr                   (* !e *)
 
-(* type pour les expressions booléennes *)
-and ebool =
-  | And of ebool*ebool
-  | Or of ebool*ebool
-  | Not of ebool
+  | If of expr*expr*expr         (* if b then e1 else e2 *)
+
+  | And of expr*expr
+  | Or of expr*expr
+  | Not of expr
 
   | Leq of expr*expr
   | Geq of expr*expr
@@ -47,7 +45,7 @@ type value =
   | VRec of string*var*env*expr
   | VUnit
   | VRef of address
-  (*| VBoo of bool*)
+  | VBoo of bool
   | VStdLib of (env -> value mem -> expr -> value)
 and env = (var*value) list
 
@@ -63,40 +61,13 @@ let rec affiche_expr e =
 	print_string ")"
       end
   in
-  let rec affiche_bool = function
-    | And(a,b) -> begin
-        print_string "And(" ;
-        affiche_bool a ;
-        print_string ", " ;
-        affiche_bool b ;
-        print_string ")"
-      end
-    | Or(a,b) -> begin
-        print_string "Or(" ;
-        affiche_bool a ;
-        print_string ", " ;
-        affiche_bool b ;
-        print_string ")"
-      end
-    | Not b -> begin
-        print_string "Not " ;
-        affiche_bool b ;
-      end
-    | Leq(e1,e2) -> aff_aux "Leq(" e1 e2
-    | Geq(e1,e2) -> aff_aux "Geq(" e1 e2
-    | Lt(e1,e2) -> aff_aux "Lt(" e1 e2
-    | Gt(e1,e2) -> aff_aux "Gt(" e1 e2
-    | Eq(e1,e2) -> aff_aux "Eq(" e1 e2
-    | Neq(e1,e2) -> aff_aux "Neq(" e1 e2
-    | True -> print_string "True"
-    | False -> print_string "False"
-  in
   match e with
   | Const k -> print_int k
   | Add(e1,e2) -> aff_aux "Add(" e1 e2
   | Mul(e1,e2) -> aff_aux "Mul(" e1 e2
   | Min(e1,e2) -> aff_aux "Min(" e1 e2
   | Div(e1,e2) -> aff_aux "Div(" e1 e2
+
   | Var x -> print_string x
   | Let(x,e1,e2) -> aff_aux ("Let(" ^ x  ^ ", ") e1 e2
   | Fun(x,e) -> begin
@@ -110,11 +81,7 @@ let rec affiche_expr e =
       print_string ")" ;
     end
   | App(e1,e2) -> aff_aux "App(" e1 e2 ;
-  | If(b,e1,e2) -> begin
-      print_string "If(" ;
-      affiche_bool b ;
-      aff_aux ", " e1 e2
-    end
+
   | Unit -> print_string "Unit"
   | Seq(e1,e2) -> aff_aux "Seq(" e1 e2
   | Aff(e1,e2) -> aff_aux "Aff(" e1 e2
@@ -123,6 +90,27 @@ let rec affiche_expr e =
       affiche_expr e ;
       print_string ")"
     end
+
+  | If(b,e1,e2) -> begin
+      print_string "If(" ;
+      affiche_expr b ;
+      aff_aux ", " e1 e2
+    end
+  | And(a,b) -> aff_aux "And(" a b ;
+  | Or(a,b) -> aff_aux "Or(" a b
+  | Not b -> begin
+      print_string "Not(" ;
+      affiche_expr b ;
+      print_string ")"
+    end
+  | Leq(e1,e2) -> aff_aux "Leq(" e1 e2
+  | Geq(e1,e2) -> aff_aux "Geq(" e1 e2
+  | Lt(e1,e2) -> aff_aux "Lt(" e1 e2
+  | Gt(e1,e2) -> aff_aux "Gt(" e1 e2
+  | Eq(e1,e2) -> aff_aux "Eq(" e1 e2
+  | Neq(e1,e2) -> aff_aux "Neq(" e1 e2
+  | True -> print_string "True"
+  | False -> print_string "False"
 
 
 exception Div_by_Zero
@@ -133,6 +121,10 @@ exception Unbound of string
 let ( !. ) v = match v with
   | VInt(i) -> i
   | _ -> raise (Not_expected "un entier")
+
+let ( !? ) v = match v with
+  | VBoo(a) -> a
+  | _ -> raise (Not_expected "une booléen")
 
 let ( !! ) v = match v with
   | VRef(a) -> a
@@ -155,10 +147,12 @@ let rec eval env m = function
       raise Div_by_Zero
     else
       VInt(!.(eval env m e1) / deno)
+
   | Var x -> find x env
   | Let(x,e1,e2) ->
     let v = eval env m e1 in
-    eval ((x,v)::env) m e2
+    let env'  = if x = "_" then env else (x,v)::env in
+    eval env' m e2
   | Fun(x,e) -> VFun(x,env,e)
   | FunRec(f,x,e) -> VRec(f, x, env, e)
   | App(e1,e2) -> begin
@@ -170,7 +164,7 @@ let rec eval env m = function
       | VStdLib(f) -> f env m e2 ;
       | _ -> raise App_not_fun
     end
-  | If(b,e1,e2) -> if eval_bool env m b then eval env m e1 else eval env m e2
+
   | Unit -> VUnit
   | Seq(e1,e2) -> begin
       ignore (eval env m e1) ;
@@ -182,18 +176,20 @@ let rec eval env m = function
       set_mem m a v ;
       VUnit
     end
-  | Der(e) ->  let a = !! (eval env m e) in
-    try get_mem m a
-    with Not_found -> raise (Unbound "reference")
-and eval_bool env m = function
-  | And(a,b) -> (eval_bool env m a) && (eval_bool env m b)
-  | Or(a,b) -> (eval_bool env m a) || (eval_bool env m b)
-  | Not b -> not (eval_bool env m b)
-  | Leq(e1,e2) -> (eval env m e1) <= (eval env m e2)
-  | Geq(e1,e2) -> (eval env m e1) >= (eval env m e2)
-  | Lt(e1,e2) -> (eval env m e1) < (eval env m e2)
-  | Gt(e1,e2) -> (eval env m e1) > (eval env m e2)
-  | Eq(e1,e2) -> (eval env m e1) = (eval env m e2)
-  | Neq(e1,e2) -> (eval env m e1) <> (eval env m e2)
-  | True -> true
-  | False -> false
+  | Der(e) ->  begin
+      let a = !! (eval env m e) in
+      try get_mem m a
+      with Not_found -> raise (Unbound "reference")
+    end
+  | If(b,e1,e2) -> if !?(eval env m b) then eval env m e1 else eval env m e2
+  | And(a,b) -> VBoo ( !?(eval env m a) && !?(eval env m b) )
+  | Or(a,b) -> VBoo ( !?(eval env m a) || !?(eval env m b) )
+  | Not b -> VBoo ( not !?(eval env m b) )
+  | Leq(e1,e2) -> VBoo ( !?(eval env m e1) <= !?(eval env m e2) )
+  | Geq(e1,e2) -> VBoo ( !?(eval env m e1) >= !?(eval env m e2) )
+  | Lt(e1,e2) -> VBoo ( !?(eval env m e1) < !?(eval env m e2) )
+  | Gt(e1,e2) -> VBoo ( !?(eval env m e1) > !?(eval env m e2) )
+  | Eq(e1,e2) -> VBoo ( !?(eval env m e1) = !?(eval env m e2) )
+  | Neq(e1,e2) -> VBoo ( !?(eval env m e1) <> !?(eval env m e2) )
+  | True -> VBoo true
+  | False -> VBoo false
