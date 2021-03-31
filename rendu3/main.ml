68a1c1f48da535b4ce6d839fb2d0c7a45fafb6dc
vecteurNabla <nicolas.nardino@club-internet.fr>
2021-03-31 14:40:19 +0200

    Là la deuxième partie du autotest est ok

--- rendu3/main.ml
+++ rendu3/main.ml
@@ -144,12 +144,15 @@ let exec () =
         print_newline ();
         if 0 = Sys.command
                  ("[ \"$(printf \"%b\n%b\n%b\n%b\" "
-                  ^ "\"let prInt i = print_int i; print_newline (); i\n;;\n\" "
+                  ^ "\"let prInt i k = print_int i; print_newline (); k i\n;;\n\" "
                   ^ "\"let " ^ (Transformation.(~+) "fst = fst\n;;\n\" ")
                   ^ "\"let " ^ (Transformation.(~+) "snd = snd\n;;\n\" ")
-                  ^  (if !std_input then "\"" ^ in_from_stdin ^"\""
+                  ^  (if !std_input then
+                        "\"$(printf \"%b\" \""
+                        ^ in_from_stdin
+                        ^ "\" | ./fouine -cps -outcode -stdin)\""
                       else
-                        "\"$(cat " ^ !nom_fichier ^ ")\""
+                        "\"$(./fouine -cps -outcode " ^ !nom_fichier ^ ")\""
                      ) ^ " | ocaml -stdin)\" = \"$("
                   ^  (if !std_input then "printf \"%b\n\" \""
                                          ^ in_from_stdin
